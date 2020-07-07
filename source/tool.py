24423ab2365de40ed22da0ec1f722deb41485216
Juan Rodriguez <juanrm071299@gmail.com>
2020-07-06 18:45:41 -0600

    Create a title
    add more constants
    add a title and subtitle with functions

--- source/tool.py
+++ source/tool.py
@@ -6,18 +6,31 @@ from . import constants as c
 class Control():
     def __init__(self):
         self.screen = pg.display.get_surface()
-        self.crashed = False
+        self.gameExit = False
         self.clock = pg.time.Clock()
         self.fps = 60
 
     def event_loop(self):
         for event in pg.event.get():
             if event.type == pg.QUIT:
-                self.crashed = True
+                self.gameExit = True
+
+    def title_game(self):
+        #Title
+        self.myfont1 = pg.font.SysFont("monospace", c.FONT_SIZE)
+        self.label_title = self.myfont1.render("BATTLE", 1, c.LIGHTYELLOW)
+        SCREEN.blit(self.label_title, (c.SCREEN_WIDTH*0.35, c.SCREEN_HEIGHT*0.05))
+        #Subtitle
+        self.myfont2 = pg.font.SysFont("monospace", c.FONT_SIZE//2)
+        self.label_subtitle = self.myfont2.render("Avatars vs Rooks", 1, c.LIGHTYELLOW)
+        SCREEN.blit(self.label_subtitle, (c.SCREEN_WIDTH*0.30, c.SCREEN_HEIGHT*0.15))
+
     def main(self):
-        while not self.crashed:  
+        while not self.gameExit:  
             self.event_loop()
-            pg.display.update()
+            SCREEN.fill(c.SKY_BLUE)   #<---Intentar hacer todo esto en una funcion y 
+            self.title_game()
+            pg.display.update()       #  ver todavia mejor si se puede hacer desde archivos diferentes
             self.clock.tick(self.fps)
 
 
